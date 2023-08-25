b617969d37aedad83024c5f79375a7722ec20b60
el-sunais6167 <yelsunais2014@gmail.com>
2023-08-25 21:32:44 +0100

    update-prod

---
+++ project/update/app/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+admin.site.register(User_admin)
+admin.site.register(Station)
+admin.site.register(Patient)
+admin.site.register(Diagnosis)
+admin.site.register(Treatment)
+admin.site.register(Morbidity)
+admin.site.register(Mortality)
+admin.site.register(Station_admin)
+admin.site.register(Profile)
+admin.site.register(Campaign)
+admin.site.register(CampReport)
+admin.site.register(Household)
+admin.site.register(Preventive)
